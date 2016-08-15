b4d70e848a1f43d800214e2310e3220b4f8fb745
砚然 <yanran.wwj@alipay.com>
2016-08-15 17:49:18 +0800

    基于koa构建简单的server类

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const koa = require('koa');
+const app = koa();
+const router = require('koa-router')();
+const DEFAULT_PORT = 3000;
+
+router.post('/test/getuser', function *(next) {
+  console.log('request :', this.request);
+  this.response.body = 'something in post';
+  console.log('response :', this.response);
+});
+
+router.get('/test', function *(next) {
+  console.log('request in get:', this.request);
+  this.response.body = 'something';
+});
+
+app
+  .use(router.routes())
+  .listen(DEFAULT_PORT);
+
+console.log('Now listening on port :' + DEFAULT_PORT);
+
